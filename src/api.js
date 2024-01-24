517da5b8d6e0912585b7ef77267fb951c3323b3a
Jahanvi Dubey <dubeyjahanvi07@gmail.com>
2024-01-24 23:37:07 +0530

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+	method: "GET",
+	headers: {
+	  "X-RapidAPI-Key": "17d3eb8a70mshcdb9fe39606a75dp1399d8jsn4d7c67c66dce",
+	  "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+	},
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "a8ce7655bd3d35f16c8c9dacee8361f6"; 
